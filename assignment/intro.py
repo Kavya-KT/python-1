56ecb543aa072c8a045341378811be69adc5baf2
KAVYA KT <162782227+Kavya-KT@users.noreply.github.com>
2026-07-10 13:00:01 +0530

    Add files via upload

---
+++ assignment/intro.py
@@ -0,0 +1,281 @@
+#python
+# print('python')
+# print('kavya')
+# print("python")
+# print('5')
+
+# hsvsxbjsbxysxjsxbhs
+#123name='kavya Kt'#error
+
+#comments
+'''
+gdh
+hdjd
+hddjd
+dhdjkd
+dhjd
+'''
+
+# name123='kavya'
+# _name='kavya'
+
+#print(name)
+
+
+#print(name)
+
+# type function
+# name='kavya'
+# print(type(name))
+
+# age=10
+# print(type(age))
+
+# salary=10.515
+# print(type(salary))
+
+# age1='10'
+# print(age1)
+# print(type(age1))
+
+# cntrl+/
+
+# age=50
+# print(age)
+
+#user input
+
+# name=input('enter you name=')
+# print(name)
+# print(type(name))
+
+# age=input('enter you age=')
+# print(age)
+# print(type(age))
+# b=int(age)
+# print(type(b))
+
+# age=int(input('enter your age='))
+
+#flaot()
+# salary=float(input('enetr your salary'))
+# print(salary)
+
+#boolean datatype
+# a=True
+# b=False
+# print(type(a))
+
+
+#string concatination
+# print('hello'+'world')
+# print('hello '+'world')
+# # print(5+3)
+# # print('5'+'3')
+
+# # name='kavya'
+# # age=10
+# # print('my name is '+name)
+# # print('my name is ',name)
+
+# # print(age , 'years old')
+
+# # print('hello')
+# # print('world')
+
+
+# #new line
+# print('hello\nworld')
+# #print('hello\n\nworld')
+
+# #tab space
+# print('hello\tworld')
+# print('hello\t\tworld')
+
+# operators
+
+#1.arithemetic
+
+
+# a=10
+# b=3
+# print(a%b)
+# print(a**2)
+
+# a=int(input('enter your number1='))
+
+# x=5
+# print(x)
+
+# x*=2
+# print(x)
+
+# x=100
+# y=100
+# print(x>=y)
+
+#logical operator
+
+#and
+'''
+T T-->True
+T F-->Falsef
+F F--> F
+'''
+
+#or
+'''
+T F-->T
+T T-->T
+F F-->F
+'''
+# x=5
+# y=10
+
+# print(x>10 and y<2)
+# print(x>1 and y<12)
+# print(x>10 and y<12)
+
+# a=10
+# b=20
+
+
+# print('msks')# string
+# print(5)# int
+# print(5.684)# float
+
+#variable
+# Name='kavya'
+# name123
+
+#input
+# name='kavya kt'
+# print(name)
+
+#type
+
+# a='10.25'
+# print(type(a))
+
+# #input
+# name=input('enter your name=')
+# print(name)
+
+
+# age=float(input('enter your age='))
+# print(age)
+# print(type(age))
+#int
+#float
+
+#opertors
+#1.arithemetic
+# a=10
+# b=3
+# print(a**2)
+
+#2.assignment
+# x=5
+
+
+# print(x)
+# x/=2
+# print(x)
+
+#3.comparison
+# a=10
+# b=10
+# print(a>b)
+
+#logical 
+#and,or,not
+#and
+
+# T T-->True
+# T f-->False
+# f f--> False
+
+# or
+
+# t f-->T
+# f f--> f
+# T T--> t
+
+# a=10
+# b=5
+# c=3
+
+# print(not(a>b or b<c))
+
+#4.memmbership
+#in ,not in
+# a='string'
+# print('i' in a)
+# print('z'not  in a)
+
+#identity
+#is is not
+# a='string'
+# b='string'
+# print(a==b)
+# print(a is b)
+# print(id(a))
+# print(id(b))
+
+# a=[1,2,3,4]
+# b=[1,2,3]
+# print(a==b)
+# print(a is b)
+# print(id(a))
+# print(id(b))
+
+
+# conditional statements
+
+#voting eligibity check
+# age=int(input('enter your age='))
+# if condition:
+#     body 
+# if age>=18:
+#     print('success')
+
+# else:
+#     print('age less than 18')
+
+#? check a number is +ve,-ve or zero
+# num=0-->Zero
+# num>0-->+ve numver
+# num<0-->-ve number
+
+# num=int(input('enter you number='))
+# if num>0:
+#     print('+ve')
+# elif num<0:
+#     print('-ve')
+# else:
+#     print('zero')
+
+#nested if
+
+# marks=int(input('enter your mark='))
+
+# if marks>=35:
+#     if marks>=90:
+#         print('A grade')
+#     else:
+#         print('pass')
+    
+# else:
+#     print('fail')
+
+num=int(input('enter number'))
+if num%2==0:
+    print('even')
+else:
+    print('odd')
+
+
+
+
+
+
